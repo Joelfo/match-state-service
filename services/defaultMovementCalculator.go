b7076f8808e4bd31923563667df9ca29e6677280
Joel Lopes <joellopessouza@id.uff.br>
2025-05-18 18:01:40 -0300

    Get Bishop possible movements logic (#3)

--- services/defaultMovementCalculator.go
+++ services/defaultMovementCalculator.go
@@ -5,6 +5,8 @@ import "github.com/joelfo/match-state-service/models/game"
 type DefaultMovementCalculator struct {
 }
 
+var boardSize = 8
+
 func (calculator *DefaultMovementCalculator) GetRookPossibleMovements(rook *game.Piece, board *game.Board) []*game.Movement {
 	currentPos := board.GetPiecePosition(rook)
 
@@ -58,6 +60,40 @@ func (calculator *DefaultMovementCalculator) GetRookPossibleMovements(rook *game
 	return possibleMovements
 }
 
-func (calculator *DefaultMovementCalculator) GetBishopPossibleMovements(rook *game.Piece, board *game.Board) []*game.Movement {
-	return []*game.Movement{}
+func (calculator *DefaultMovementCalculator) GetBishopPossibleMovements(bishop *game.Piece, board *game.Board) []*game.Movement {
+	currentPos := board.GetPiecePosition(bishop)
+
+	var possibleMovements []*game.Movement
+
+	directions := []struct{ dx, dy int }{
+		{1, 1},
+		{1, -1},
+		{-1, 1},
+		{-1, -1},
+	}
+
+	for _, direction := range directions {
+		for i := 1; i < boardSize; i++ {
+			xPos := currentPos.X + direction.dx*i
+			yPos := currentPos.Y + direction.dy*i
+
+			if xPos > boardSize || yPos > boardSize || xPos < 0 || yPos < 0 {
+				break
+			}
+
+			movement := board.GetMovementTo(bishop, xPos, yPos)
+
+			if movement == nil {
+				break
+			}
+
+			possibleMovements = append(possibleMovements, movement)
+
+			if movement.CapturedPiece != nil {
+				break
+			}
+		}
+	}
+
+	return possibleMovements
 }
