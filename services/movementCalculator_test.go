cb6578d0948c8252bed344b3f2886614afc74a3c
Joel Lopes <joellopessouza@id.uff.br>
2025-05-18 17:25:21 -0300

    Update movementCalculator_test.go

--- services/movementCalculator_test.go
+++ services/movementCalculator_test.go
@@ -52,3 +52,56 @@ func TestGeralRulesOnPieceMovements(t *testing.T) {
 		}
 	}
 }
+
+func TestGetRookPossibleMovements(t *testing.T) {
+	calculator, err := getMovementCalculator()
+
+	if err != nil {
+		t.Fatalf("Error during Movement Calculator instantiation: %f", err)
+	}
+
+	board := getSetUpBoard(t)
+	if board == nil {
+		t.Errorf("Board is nil")
+		return
+	}
+	whiteRook := (*board).GetPieceAt(0, 0)
+	possibleMovements := calculator.GetRookPossibleMovements(whiteRook, board)
+
+	if len(possibleMovements) > 0 {
+		t.Errorf("Rook possible moviments in board just set up are wrong")
+	}
+
+	board = game.NewEmptyBoard()
+	board.SetPieceAt(whiteRook, 2, 3)
+
+	whiteBishop := &game.Piece{Color: teamColors.White, Type: pieceType.Bishop}
+	board.SetPieceAt(whiteBishop, 2, 0)
+
+	blackPawn := &game.Piece{Color: teamColors.Black, Type: pieceType.Pawn}
+	board.SetPieceAt(blackPawn, 4, 3)
+
+	whiteKnight := &game.Piece{Color: teamColors.White, Type: pieceType.Knight}
+	board.SetPieceAt(whiteKnight, 1, 3)
+
+	possibleMovements = calculator.GetRookPossibleMovements(whiteRook, board)
+
+	for _, movement := range possibleMovements {
+		if movement.FinalPos.Y != 3 && movement.FinalPos.X != 2 {
+			t.Errorf("Rook does not respect rook movements limitation")
+		}
+		if movement.FinalPos.Y == 3 && movement.FinalPos.X == 0 {
+			t.Errorf("Rook can't move through white pieces.")
+		}
+		if movement.FinalPos.Y == 3 && movement.FinalPos.X > 4 {
+			t.Errorf("Rook can't move through black pieces.")
+		}
+	}
+
+	if len(possibleMovements) < 8 {
+		t.Errorf("Rook calculated movements are less than real possible movements.")
+	} else if len(possibleMovements) > 8 {
+		t.Errorf("Rook calculated movements are more than real possible movements.")
+	}
+
+}
